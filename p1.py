597a03cbd003acb48615cebc25f693db810fc899
PaulFSherwood <PaulFSherwood@gmail.com>
2023-03-08 21:25:31 -0700

    test push

--- p1.py
+++ p1.py
@@ -1,3 +1,27 @@
+####################################################################################
+#  __  __            _     _            
+# |  \/  | __ _  ___| |__ (_)_ __   ___ 
+# | |\/| |/ _` |/ __| '_ \| | '_ \ / _ \
+# | |  | | (_| | (__| | | | | | | |  __/
+# |_|  |_|\__,_|\___|_| |_|_|_| |_|\___|                                      
+#  _                          _             
+# | |    ___  __ _ _ __ _ __ (_)_ __   __ _ 
+# | |   / _ \/ _` | '__| '_ \| | '_ \ / _` |
+# | |__|  __/ (_| | |  | | | | | | | | (_| |
+# |_____\___|\__,_|_|  |_| |_|_|_| |_|\__, |
+#                                     |___/ 
+#
+# Author: Sentdex
+# Video: Neural Networks from Scratch - P.1 The Dot Product
+# Website: https://www.youtube.com/watch?v=Wo5dMEP_BbI&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3&index=1
+# Date: March 8th, 2023
+# Purpose: Machine Learning training
+#
+# Notes:
+# - This script requires Python 3.7 or later.
+# - Make sure to install the necessary dependencies before running.
+####################################################################################
+
 import sys
 import numpy as np
 import matplotlib
