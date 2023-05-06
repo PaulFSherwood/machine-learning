0fe9b9b308821fa0dd1762ac2f17472fa48f252e
Paul Sherwood <paulfsherwood@gmail.com>
2023-05-05 21:09:01 -0600

    softmad video 6

--- src/video06.py
+++ src/video06.py
@@ -1,9 +1,15 @@
 import numpy as np
+import math
 
+
+
+# INPUT
 layer_output = [4.8, 1.21, 2.385]
 
-E = 2.71828182846
+# E = 2.71828182846
+E = math.e
 
+# EXPONENTIATE
 exp_values = np.exp(layer_output) #[]
 
 # for output in layer_output:
@@ -11,11 +17,17 @@ exp_values = np.exp(layer_output) #[]
 
 print(exp_values)
 
-norm_base = sum(exp_values)
-norm_values = []
+# norm_base = sum(exp_values)
+# norm_values = []
 
-for value in exp_values:
-    norm_values.append(value / norm_base)
+# for value in exp_values:
+#     norm_values.append(value / norm_base)
+
+# NORMALIZE
+norm_values = exp_values / np.sum(exp_values)
+
+# EXPONENTIATION AND NORMALIZATION = SOFTMAX
+
 
 print(norm_values)
 print(sum(norm_values))
