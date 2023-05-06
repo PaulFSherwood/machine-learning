af96e54d98715a42af43e21a9af14165229c46b7
Paul Sherwood <paulfsherwood@gmail.com>
2023-05-06 02:42:22 -0600

    i added this file but didn't pay attention to the video, starting over.

--- src/video06.py
+++ src/video06.py
@@ -1,33 +1,51 @@
+# import numpy as np
+# import math
+
+
+
+# # INPUT
+# layer_output = [4.8, 1.21, 2.385]
+
+# # E = 2.71828182846
+# E = math.e
+
+# # EXPONENTIATE
+# exp_values = np.exp(layer_output) #[]
+
+# # for output in layer_output:
+# #     exp_values.append(E ** output)
+
+# print(exp_values)
+
+# # norm_base = sum(exp_values)
+# # norm_values = []
+
+# # for value in exp_values:
+# #     norm_values.append(value / norm_base)
+
+# # NORMALIZE
+# norm_values = exp_values / np.sum(exp_values)
+
+# # EXPONENTIATION AND NORMALIZATION = SOFTMAX
+
+
+# print(norm_values)
+# print(sum(norm_values))
+
 import numpy as np
-import math
+import nnfs
 
+nnfs.init()
 
+layer_ouputs = [[4.8, 1.21, 2.385],
+                [8.9, -1.81, 0.2],
+                [1.41, 1.051, 0.026]]
 
-# INPUT
-layer_output = [4.8, 1.21, 2.385]
+exp_values = np.exp(layer_ouputs)
 
-# E = 2.71828182846
-E = math.e
+# print(np.sum(layer_ouputs, axis=1, keepdims=True))
 
-# EXPONENTIATE
-exp_values = np.exp(layer_output) #[]
-
-# for output in layer_output:
-#     exp_values.append(E ** output)
-
-print(exp_values)
-
-# norm_base = sum(exp_values)
-# norm_values = []
-
-# for value in exp_values:
-#     norm_values.append(value / norm_base)
-
-# NORMALIZE
-norm_values = exp_values / np.sum(exp_values)
-
-# EXPONENTIATION AND NORMALIZATION = SOFTMAX
-
+norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
 print(norm_values)
-print(sum(norm_values))
+# print(sum(norm_values))
